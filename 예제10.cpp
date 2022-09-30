51fb3180be9ec3c146b3e237e277ca235774a655
NANONANDFLASH <68502584+NANONANDFLASH@users.noreply.github.com>
2022-09-30 13:52:22 +0900

    Add files via upload

--- 예제10.cpp
+++ 예제10.cpp
@@ -1,16 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
-	int n1, n2, i;
-	printf("두 수 입력 : ");
-	scanf("%d %d", &n1, &n2);
-	if (n1 < n2){
-		for(i=n1;i<=n2;i++)
-		printf("%d ", i);
-	}
-	else{
-		for(i=n2;i<=n1;i++)
-			printf("%d ", i);
-		
-	}
-} 
+	int i=10;
+	
+	do{
+		printf("%d\n", i);
+		i--; 
+	}while(i>=5);
+}
--- 예제11.cpp
+++ 예제11.cpp
@@ -1,14 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
-	int num1, num2, i;
+	int n, i=0;
+	printf("정수입력: ");
+	scanf(" %d", &n);
 	
-	printf("공배수를 구할 두 정수 입력 : ");
-	scanf(" %d %d", &num1, &num2);
-	
-	for (i = 1; i <= 100; i++){
-		if (i % num1 == 0 && i % num2 == 0)
-				printf("%d ", i);
-}
-
+	do{
+		printf("I love(?) progrmming\n", i);
+		i++; 
+	}while(i<n);
 }
