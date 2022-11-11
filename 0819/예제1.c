a7749f65bf35a431ce4221e63008c982e4bfeac9
NANONANDFLASH <68502584+NANONANDFLASH@users.noreply.github.com>
2022-11-11 14:10:01 +0900

    Add files via upload

---
+++ 0819/예제1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void){
+	/*
+	n이 0일때 
+	int n = 0;
+	while(n<10){
+		printf("#");
+		n++;
+	}
+	*/
+	//n이 1일때 
+	int n = 1;
+	while(n=<10){
+		printf("#");
+		n++;
+	}
+}
